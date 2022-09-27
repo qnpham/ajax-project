43c9179b10d74ce94883a9801309bcebc7dac358
Quan Pham <mockanterr@gmail.com>
2022-09-27 23:04:34 +0000

    make search result view gets displayed after the user submits a value #5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-var $form = document.querySelector('form');
+var $form = document.querySelector('#home-form');
+var $homePage = document.querySelector('[data-view="home-page"]');
+var $nav = document.querySelector('[data-view="nav"]');
+var $navHeader = document.querySelector('#nav-header');
+
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
+  $homePage.classList.add('hidden');
+  $nav.classList.remove('hidden');
+});
+
+$navHeader.addEventListener('click', function (event) {
+  $homePage.classList.remove('hidden');
+  $nav.classList.add('hidden');
 });
