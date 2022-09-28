780c342f4533fa2f0e5dfd850b9717101629c611
Quan Pham <mockanterr@gmail.com>
2022-09-28 01:44:40 +0000

    retrieve all movies with keyword from api and store it in data model. #7

--- js/main.js
+++ js/main.js
@@ -2,14 +2,32 @@ var $form = document.querySelector('#home-form');
 var $homePage = document.querySelector('[data-view="home-page"]');
 var $nav = document.querySelector('[data-view="nav"]');
 var $navHeader = document.querySelector('#nav-header');
-
-$form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  $homePage.classList.add('hidden');
-  $nav.classList.remove('hidden');
-});
+var $searchResult = document.querySelector('[data-view="search-result"]');
 
 $navHeader.addEventListener('click', function (event) {
   $homePage.classList.remove('hidden');
   $nav.classList.add('hidden');
+  $searchResult.classList.add('hidden');
 });
+
+$form.addEventListener('submit', function (event) {
+  var $homeInput = document.querySelector('#home-input');
+  var keyword = $homeInput.value;
+  event.preventDefault();
+  $homePage.classList.add('hidden');
+  $nav.classList.remove('hidden');
+  getApi(keyword);
+
+});
+
+function getApi(keyword) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://omdbapi.com/?apikey=e9abc53b&s=' + keyword);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    for (var i = 0; i < xhr.response.Search.length; i++) {
+      data.searchResult.push(xhr.response.Search[i]);
+    }
+  });
+  xhr.send();
+}
